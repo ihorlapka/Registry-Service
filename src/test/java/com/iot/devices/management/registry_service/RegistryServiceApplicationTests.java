923cdce65ca77f52d858523bd0795849a40a8907
Ihor <53528174+ihorlapka@users.noreply.github.com>
2025-06-19 19:48:10 +0300

    Add files via upload
    initial commit

---
+++ src/test/java/com/iot/devices/management/registry_service/RegistryServiceApplicationTests.java
@@ -0,0 +1,15 @@
+package com.iot.devices.management.registry_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+@Import(TestcontainersConfiguration.class)
+@SpringBootTest
+class RegistryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
