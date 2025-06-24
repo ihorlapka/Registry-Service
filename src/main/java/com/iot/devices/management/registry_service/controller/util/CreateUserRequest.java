9138ed1a997e72f7d239040291691715570abd76
ihor <tailihor92@gmail.com>
2025-06-24 13:06:23 +0300

    added validation to CreateUserRequest, added tests

--- src/main/java/com/iot/devices/management/registry_service/controller/util/CreateUserRequest.java
+++ src/main/java/com/iot/devices/management/registry_service/controller/util/CreateUserRequest.java
@@ -2,12 +2,31 @@ package com.iot.devices.management.registry_service.controller.util;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.NonNull;
 
 public record CreateUserRequest(
-        @NonNull @NotBlank String firstName,
-        @NonNull @NotBlank String lastName,
-        @NonNull @NotBlank String phone,
-        @NonNull @Email String email,
-        @NonNull @NotBlank String address) {
+        @NonNull
+        @NotBlank(message = "First name is required")
+        String firstName,
+
+        @NonNull
+        @NotBlank(message = "Last name is required")
+        String lastName,
+
+        @NonNull
+        @NotBlank(message = "Phone is required")
+        @Pattern(regexp = "^\\+?[0-9\\- ]{7,20}$", message = "Phone must be a valid number")
+        String phone,
+
+        @NonNull
+        @NotBlank(message = "Email is required")
+        @Email(message = "Email must be valid")
+        String email,
+
+        @NonNull
+        @NotBlank(message = "Address is required")
+        @Size(min = 5, max = 255, message = "Address must be between 5 and 255 characters")
+        String address) {
 }
--- src/test/java/com/iot/devices/management/registry_service/controller/UserControllerTest.java
+++ src/test/java/com/iot/devices/management/registry_service/controller/UserControllerTest.java
@@ -2,23 +2,32 @@ package com.iot.devices.management.registry_service.controller;
 
 import com.iot.devices.management.registry_service.persistence.model.User;
 import com.iot.devices.management.registry_service.persistence.repos.UsersRepository;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.dao.DataAccessResourceFailureException;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.Optional;
+
+import static org.hibernate.internal.util.collections.CollectionHelper.listOf;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest(UserController.class)
+@WebMvcTest(controllers = {
+        UserController.class,
+        ErrorHandler.class
+})
 class UserControllerTest {
-
 
     @Autowired
     MockMvc mockMvc;
@@ -26,9 +35,6 @@ class UserControllerTest {
     @MockitoBean
     UsersRepository usersRepository;
 
-    @BeforeEach
-    void setUp() {
-    }
 
     String firstName = "John";
     String lastName = "Doe";
@@ -46,9 +52,12 @@ class UserControllerTest {
             }
             """.formatted(firstName, lastName, phone, email, address);
 
+
+    User USER = new User(1L, firstName, lastName, email, phone, address);
+
     @Test
     void createUser() throws Exception {
-        when(usersRepository.save(any())).thenReturn(new User(1L, firstName, lastName, email, phone, address));
+        when(usersRepository.save(any())).thenReturn(USER);
         mockMvc.perform(post("/api/v1/users")
                         .contentType(APPLICATION_JSON)
                         .content(json))
@@ -58,22 +67,107 @@ class UserControllerTest {
     }
 
     @Test
-    void patchUser() {
+    void duplicatedUser() throws Exception {
+        when(usersRepository.findByEmail(email)).thenReturn(Optional.of(USER));
+        mockMvc.perform(post("/api/v1/users")
+                        .contentType(APPLICATION_JSON)
+                        .content(json))
+                .andExpect(status().isConflict());
+        verify(usersRepository).findByEmail(email);
+        verifyNoMoreInteractions(usersRepository);
     }
 
     @Test
-    void getAllUsers() {
+    void dbIsDown() throws Exception {
+        when(usersRepository.findByEmail(email)).thenThrow(new DataAccessResourceFailureException("Db is down!"));
+        mockMvc.perform(post("/api/v1/users")
+                        .contentType(APPLICATION_JSON)
+                        .content(json))
+                .andExpect(status().isInternalServerError());
+        verify(usersRepository).findByEmail(email);
+        verifyNoMoreInteractions(usersRepository);
     }
 
     @Test
-    void getUserById() {
+    void patchUser() throws Exception {
+        String patchedAddress = "some updated address";
+        String patch = """
+            {
+              "id": "%s",
+              "address"  : "%s"
+            }
+            """.formatted(1, patchedAddress);
+        when(usersRepository.findById(1L)).thenReturn(Optional.of(USER));
+        USER.setAddress(patchedAddress);
+        when(usersRepository.save(any())).thenReturn(USER);
+        mockMvc.perform(patch("/api/v1/users")
+                        .contentType(APPLICATION_JSON)
+                        .content(patch))
+                .andExpect(status().isOk());
+        verify(usersRepository).findById(1L);
+        verify(usersRepository).save(any(User.class));
+        verifyNoMoreInteractions(usersRepository);
     }
 
     @Test
-    void findByEmail() {
+    void patchNoUserFound() throws Exception {
+        String patchedAddress = "some updated address";
+        String patch = """
+            {
+              "id": "%s",
+              "address"  : "%s"
+            }
+            """.formatted(1, patchedAddress);
+        when(usersRepository.findById(1L)).thenReturn(Optional.empty());
+        mockMvc.perform(patch("/api/v1/users")
+                        .contentType(APPLICATION_JSON)
+                        .content(patch))
+                .andExpect(status().isNotFound());
+        verify(usersRepository).findById(1L);
+        verifyNoMoreInteractions(usersRepository);
     }
 
     @Test
-    void deleteUser() {
+    void getAllUsers() throws Exception {
+        when(usersRepository.findAll(any(Pageable.class))).thenReturn(new PageImpl<>(listOf(USER)));
+        mockMvc.perform(get("/api/v1/users")
+                        .contentType(APPLICATION_JSON)
+                        .content(json))
+                .andExpect(status().isOk());
+        verify(usersRepository).findAll(any(Pageable.class));
+        verifyNoMoreInteractions(usersRepository);
+    }
+
+    @Test
+    void getUserById() throws Exception {
+        when(usersRepository.findById(anyLong())).thenReturn(Optional.of(USER));
+        mockMvc.perform(get("/api/v1/users/1")
+                        .contentType(APPLICATION_JSON)
+                        .content(json))
+                .andExpect(status().isOk());
+        verify(usersRepository).findById(any());
+        verifyNoMoreInteractions(usersRepository);
+    }
+
+    @Test
+    void findByEmail() throws Exception {
+        when(usersRepository.findByEmail(email)).thenReturn(Optional.of(USER));
+        mockMvc.perform(get("/api/v1/users/email/" + email)
+                        .contentType(APPLICATION_JSON)
+                        .content(json))
+                .andExpect(status().isOk());
+        verify(usersRepository).findByEmail(any());
+        verifyNoMoreInteractions(usersRepository);
+    }
+
+    @Test
+    void deleteUser() throws Exception {
+        when(usersRepository.removeById(anyLong())).thenReturn(1);
+        mockMvc.perform(delete("/api/v1/users/1")
+                        .contentType(APPLICATION_JSON)
+                        .content(json))
+                .andExpect(status().isNoContent());
+        verify(usersRepository).removeById(anyLong());
+        verifyNoMoreInteractions(usersRepository);
     }
 }
